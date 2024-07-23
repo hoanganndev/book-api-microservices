552d4eb6eb3307f6d9304ee8f007636077f09d23
anh.nguyen <anh.nguyen@tx-tech.com>
2024-07-23 17:27:37 +0700

    #11 Build email sending service with Brevo

---
+++ notification-service/src/main/java/com/marcus/notificationservice/controller/EmailController.java
@@ -0,0 +1,26 @@
+package com.marcus.notificationservice.controller;
+
+import com.marcus.notificationservice.dto.ApiResponse;
+import com.marcus.notificationservice.dto.request.SendEmailRequest;
+import com.marcus.notificationservice.dto.response.EmailResponse;
+import com.marcus.notificationservice.service.EmailService;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+public class EmailController {
+    EmailService emailService;
+    
+    @PostMapping("/email/send")
+    ApiResponse<EmailResponse> sendEmail(@RequestBody SendEmailRequest request){
+        return ApiResponse.<EmailResponse>builder()
+                .result(emailService.sendEmail(request))
+                .build();
+    }
+}
---
+++ notification-service/src/main/java/com/marcus/notificationservice/dto/request/SendEmailRequest.java
@@ -0,0 +1,21 @@
+package com.marcus.notificationservice.dto.request;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class SendEmailRequest {
+    Recipient to;
+    String htmlContent;
+    String subject;
+}
